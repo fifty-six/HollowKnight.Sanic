fbc6cc2376a43ba88cab5368a303ec4709963e5d
fifty-six <ybham6@gmail.com>
2021-09-13 22:33:05 -0400

    Update for 1.5

---
+++ README.md
@@ -0,0 +1,7 @@
+# Sanic
+
+Mod for Hollow Knight which allows you to change the default timescale as well 
+as also affecting the rate of timescale changes to match the timescale itself.
+
+Can be configured via a settings json (`Sanic.GlobalSettings.json`) located
+within the Hollow Knight saves folder.
--- Sanic/Sanic.cs
+++ Sanic/Sanic.cs
@@ -1,38 +1,92 @@
 ﻿using Modding;
 using System;
 using System.Collections;
+using System.Linq;
 using System.Reflection;
+using System.Runtime.CompilerServices;
+using JetBrains.Annotations;
+using Mono.Cecil.Cil;
+using MonoMod.Cil;
+using MonoMod.RuntimeDetour;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+using Vasi;
 
 namespace Sanic
 {
-    public class Sanic : Mod
+    [UsedImplicitly]
+    public class Sanic : Mod, IGlobalSettings<GlobalModSettings>, ITogglableMod
     {
-        public sealed override ModSettings GlobalSettings
-        {
-            get => _globalSettings;
-            set => _globalSettings = (GlobalModSettings) value;
-        }
-        
-        private readonly FieldInfo timeSlowedField;
-        
-        private GlobalModSettings _globalSettings = new GlobalModSettings();
+        public void OnLoadGlobal(GlobalModSettings s) => _globalSettings = s;
 
-        public override string GetVersion() => Assembly.GetExecutingAssembly().GetName().Version.ToString();
-        
-        public Sanic()
-        {
-            timeSlowedField = typeof(GameManager).GetField("timeSlowed", BindingFlags.NonPublic | BindingFlags.Instance);
+        public GlobalModSettings OnSaveGlobal() => _globalSettings;
 
+        private GlobalModSettings _globalSettings = new();
+
+        public override string GetVersion() => VersionUtil.GetVersion<Sanic>();
+
+        private static readonly MethodInfo[] FreezeCoroutines = (
+            from method in typeof(GameManager).GetMethods()
+            where method.Name.StartsWith("FreezeMoment")
+            where method.ReturnType == typeof(IEnumerator)
+            select method.GetCustomAttribute<IteratorStateMachineAttribute>() into attr
+            select attr.StateMachineType into type
+            select type.GetMethod("MoveNext", BindingFlags.NonPublic | BindingFlags.Instance)
+        ).ToArray();
+
+        private ILHook[] _coroutineHooks;
+
+        public override void Initialize()
+        {
             Time.timeScale = _globalSettings.SpeedMultiplier;
-            ModHooks.Instance.HeroUpdateHook += Instance_HeroUpdateHook;
+
+            ModHooks.HeroUpdateHook += Update;
+
             On.GameManager.SetTimeScale_float += GameManager_SetTimeScale_1;
-            On.GameManager.FreezeMoment_float_float_float_float += GameManager_FreezeMoment_1;
             On.QuitToMenu.Start += QuitToMenu_Start;
+
+            _coroutineHooks = new ILHook[FreezeCoroutines.Length];
+
+            foreach ((MethodInfo coro, int idx) in FreezeCoroutines.Select((mi, idx) => (mi, idx)))
+            {
+                _coroutineHooks[idx] = new ILHook(coro, ScaleFreeze);
+
+                LogDebug($"Hooked {coro.DeclaringType?.Name}!");
+            }
         }
 
-        private void Instance_HeroUpdateHook()
+        public void Unload()
+        {
+            foreach (ILHook hook in _coroutineHooks)
+                hook.Dispose();
+
+            Time.timeScale = 1;
+
+            ModHooks.HeroUpdateHook -= Update;
+
+            On.GameManager.SetTimeScale_float -= GameManager_SetTimeScale_1;
+            On.QuitToMenu.Start -= QuitToMenu_Start;
+        }
+
+
+        private void ScaleFreeze(ILContext il)
+        {
+            var cursor = new ILCursor(il);
+
+            cursor.GotoNext
+            (
+                x => x.MatchLdfld(out _),
+                x => x.MatchCall<Time>("get_unscaledDeltaTime"),
+                x => x.MatchAdd()
+            );
+
+            cursor.GotoNext();
+
+            cursor.EmitDelegate<Func<float>>(() => _globalSettings.SpeedMultiplier);
+
+            cursor.Emit(OpCodes.Mul);
+        }
+
+        private void Update()
         {
             if (Input.GetKeyDown(KeyCode.Alpha2))
             {
@@ -42,6 +96,7 @@ namespace Sanic
                 _globalSettings.SpeedMultiplier += 0.05f;
             }
 
+            // ReSharper disable once InvertIf
             if (Input.GetKeyDown(KeyCode.Alpha1))
             {
                 if (Math.Abs(Time.timeScale - _globalSettings.SpeedMultiplier) < Mathf.Epsilon)
@@ -51,108 +106,20 @@ namespace Sanic
             }
         }
 
-        private IEnumerator GameManager_FreezeMoment_1
-        (
-            On.GameManager.orig_FreezeMoment_float_float_float_float orig,
-            GameManager self,
-            float rampDownTime,
-            float waitTime,
-            float rampUpTime,
-            float targetSpeed
-        )
-        {
-            if ((bool) timeSlowedField.GetValue(self)) yield break;
-            
-            timeSlowedField.SetValue(self, true);
-            
-            yield return self.StartCoroutine(SetTimeScale(targetSpeed, rampDownTime));
-            
-            for (float timer = 0f; timer < waitTime; timer += Time.unscaledDeltaTime * _globalSettings.SpeedMultiplier)
-            {
-                yield return null;
-            }
-
-            yield return self.StartCoroutine(SetTimeScale(1f, rampUpTime));
-            
-            timeSlowedField.SetValue(self, false);
-        }
-
         private IEnumerator QuitToMenu_Start(On.QuitToMenu.orig_Start orig, QuitToMenu self)
         {
-            yield return null;
-            
-            UIManager ui = UIManager.instance;
-            
-            if (ui != null)
-            {
-                UIManager.instance.AudioGoToGameplay(0f);
-                UnityEngine.Object.Destroy(ui.gameObject);
-            }
+            yield return orig(self);
 
-            HeroController heroController = HeroController.instance;
-            if (heroController != null)
-            {
-                UnityEngine.Object.Destroy(heroController.gameObject);
-            }
-
-            GameCameras gameCameras = GameCameras.instance;
-            if (gameCameras != null)
-            {
-                UnityEngine.Object.Destroy(gameCameras.gameObject);
-            }
-
-            GameManager gameManager = GameManager.instance;
-            if (gameManager != null)
-            {
-                try
-                {
-                    ObjectPool.RecycleAll();
-                }
-                catch (Exception exception)
-                {
-                    Debug.LogErrorFormat("Error while recycling all as part of quit, attempting to continue regardless.");
-                    Debug.LogException(exception);
-                }
-
-                gameManager.playerData.Reset();
-                gameManager.sceneData.Reset();
-                UnityEngine.Object.Destroy(gameManager.gameObject);
-            }
-
-            Time.timeScale = _globalSettings.SpeedMultiplier;
-            
-            yield return null;
-            
-            GCManager.Collect();
-            
-            UnityEngine.SceneManagement.SceneManager.LoadSceneAsync("Menu_Title", LoadSceneMode.Single);
-        }
-
-        private IEnumerator SetTimeScale(float newTimeScale, float duration)
-        {
-            float lastTimeScale = Time.timeScale;
-            for (float timer = 0f; timer < duration; timer += Time.unscaledDeltaTime)
-            {
-                float val = Mathf.Clamp01(timer / duration);
-                SetTimeScale(Mathf.Lerp(lastTimeScale, newTimeScale, val));
-                yield return null;
-            }
-
-            SetTimeScale(newTimeScale);
-        }
-
-        private void SetTimeScale(float newTimeScale)
-        {
-            Time.timeScale = (newTimeScale <= 0.01f ? 0f : newTimeScale) * _globalSettings.SpeedMultiplier;
+            TimeController.GenericTimeScale = _globalSettings.SpeedMultiplier;
         }
 
         private void GameManager_SetTimeScale_1(On.GameManager.orig_SetTimeScale_float orig, GameManager self, float newTimeScale)
         {
-            Time.timeScale = (newTimeScale <= 0.01f ? 0f : newTimeScale) * _globalSettings.SpeedMultiplier;
+            TimeController.GenericTimeScale = (newTimeScale <= 0.01f ? 0f : newTimeScale) * _globalSettings.SpeedMultiplier;
         }
     }
 
-    public class GlobalModSettings : ModSettings
+    public class GlobalModSettings
     {
         public float SpeedMultiplier = 1.3f;
     }
